f7e1f0f103f02f4352a35a45eb3bfc64366d0b28
Zaharix <qwertykaj@mail.ru>
2021-07-31 16:54:21 +0300

    fixed and added modal window

--- src/App.vue
+++ src/App.vue
@@ -3,156 +3,127 @@
     <h2>Форма подачи заявки в отдел сервиса и качества</h2>
 
     <div class="form" id="form">
+        <div class="first">
+          <h5>Ваш филиал <span class="reqiured">*</span> </h5>
 
-      <div class="first">
-        <h5>Ваш филиал <span class="reqiured">*</span> </h5>
+          <div>
+            <select v-bind:disabled="selectDisabled" class="select" v-model="selecter">
+              <option selected disabled>Выберите город</option>
+              <option v-for="city in this.cities" v-bind:key="city">{{city}}</option>
+            </select>
+          </div>
+          <div class="check" id="checkbox">
+            <input class="input" type="checkbox"
+                   v-on:change="$emit('change', setDisabled())">
+            <label class="label" for="checkbox" >Онлайн</label>
+          </div>
 
-        <div id="select">
-          <select v-bind:disabled="selectDisabled" class="select" v-model="selecter">
-            <option selected disabled>Выберите город</option>
-            <option v-for="city in this.cities" v-bind:key="city">{{city}}</option>
-          </select>
         </div>
 
-        <div class="check" id="checkbox">
-          <input class="input" type="checkbox"
-                 v-on:change="$emit('change', setDisabled())">
-          <label class="label" for="checkbox" >Онлайн</label>
-      </div>
+        <div class="second">
 
-      </div>
-
-      <div class="second">
-
-        <h5>Тема обращения <span class="reqiured">*</span> </h5>
-          <div class="check">
+          <h5>Тема обращения <span class="reqiured">*</span> </h5>
+          <div class="check" v-for="problem in problems" :key="problem">
             <input class="input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
-                   v-model="theme">
+                   v-model="theme" :value="problem">
             <label class="label" for="flexRadioDefault1" >
-              {{ this.problems[0] }}
+              {{ problem }}
             </label>
           </div>
 
-          <div class="check">
-            <input class="input" v-model="theme" type="radio"
-                   name="flexRadioDefault" id="flexRadioDefault2">
-            <label class="label" for="flexRadioDefault1">
-              {{ this.problems[1] }}
-            </label>
-          </div>
-
-          <div class="check">
-            <input class="input" v-model="theme"
-                   type="radio" name="flexRadioDefault" id="flexRadioDefault3">
-            <label class="label" for="flexRadioDefault1">
-              {{ this.problems[2] }}
-            </label>
-          </div>
-
-          <div class="check">
-            <input class="input" v-model="theme" type="radio"
-                   name="flexRadioDefault" id="flexRadioDefault4">
-            <label class="label" for="flexRadioDefault1">
-              {{ this.problems[3] }}
-            </label>
-          </div>
-
-        <input class="control" type="text" placeholder="Другое" v-model="theme"
-               v-on:click="clickOnfield()">
-      </div>
-
-      <div class="third">
-        <div class="textarea">
-          <h5>Описание проблемы <span class="reqiured">*</span></h5>
-          <textarea class="textarea" id="textArea" rows="5"
-                    v-model="description" placeholder="Введите текст"></textarea>
-        </div>
-      </div>
-
-      <div class="forth">
-        <div class="textfileimport">
-          <h5>Загрузка документов</h5>
-
-          <p class="textfileimport">Приложите, пожалуйста, полноэкранный скриншот. <br> Это поможет быстрее решить проблему.</p>
-        </div>
-        <div >
-          <input class="fileimport" type="file" id="formFile">
+          <input class="control" type="text" placeholder="Другое" v-model="theme"
+                 v-on:click="clickOnfield()">
         </div>
 
-      </div>
+        <div class="third">
+          <div class="textarea">
+            <h5>Описание проблемы <span class="reqiured">*</span></h5>
+            <textarea class="textarea" id="textArea" rows="5"
+                      v-model="description" placeholder="Введите текст"></textarea>
+          </div>
+        </div>
 
-      <div class="fifth">
-        <button type="button" v-on:click="sendForm" v-bind:disabled="buttonDisable" class="btn">
-          Отправить
-        </button>
-      </div>
+        <div class="forth">
+          <div class="textfileimport">
+            <h5>Загрузка документов</h5>
+
+            <p>Приложите, пожалуйста, полноэкранный скриншот. <br> Это поможет быстрее решить проблему.</p>
+          </div>
+          <div >
+            <input class="fileimport" type="file" id="formFile">
+          </div>
+
+        </div>
+
+        <div class="fifth">
+          <button type="button" v-on:click="sendForm" v-bind:disabled="isDataValidate" class="btn">
+            Отправить
+          </button>
+        </div>
     </div>
 
-    <div >
-
-    </div>
+    <modal v-show="isModalVisible" @close="closeModal"/>
   </div>
-
 </template>
 
 <script>
 
 import axios from "axios";
+import modal from "@/components/modal";
+
 export default {
+  components: {
+    modal
+  },
   data() {
     return {
-      cities: "",
       problems: ['Недоволен качеством услуг',
         'Расторжение договора',
         'Не приходит письмо активации на почту',
         'Не работает личный кабинет'],
+      cities: "",
       endPointForGet: 'https://60254fac36244d001797bfe8.mockapi.io/api/v1/city',
       endPointForPost: 'https://60254fac36244d001797bfe8.mockapi.io/api/v1/send-form',
       selectDisabled: false,
-      clickOnAnotherField: false,
+      clickOnAnotherField: null,
       buttonDisable: false,
       errs: [],
       selecter: null,
       checkbox: null,
       theme: null,
       description: null,
-      successPost: null,
       isPostSuccess: null,
+      isModalVisible: false,
     }
   },
-
   created() {
     this.getCities()
   },
-  watch: {
-    '$form'() {
-      this.setButtonEnable();
+
+  computed: {
+    isDataValidate() {
+      return !((this.selecter || this.checkbox) && this.theme && this.description)
     }
   },
   methods: {
+    showModal() {
+      this.isModalVisible = true
+      this.checkbox = false
+      this.selecter = null
+      this.description = null
+      this.theme = null
+
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
     getCities() {
       axios.get(this.endPointForGet).then(res => {
-        this.cities = res.data
-        var arr = this.cities.map((obj) => {
-          return obj.title
-        })
-        this.cities = arr
+        this.cities = res.data.map(obj => obj.title)
       }).catch(err => {
         console.log('Error' + err)
       })
     },
-    setButtonEnable() {
-      console.log("checkForm")
-      if (this.selecter && this.description && this.theme) {
-        console.log('if Passed')
-        this.buttonDisable = false
-      }
-      else {
-
-        console.log("All is bad")
-      }
-    },
-
     setDisabled() {
       this.selectDisabled = !this.selectDisabled
       this.selecter = this.selectDisabled
@@ -164,18 +135,23 @@ export default {
 
     sendForm() {
       console.log('trying to send post')
-      console.log(this.selecter + '\t' + this.theme + '\t' + this.description)
       axios.post(this.endPointForPost).then(res => {
-        this.successPost = res.data
-        this.isPostSuccess = this.successPost.success
+        this.isPostSuccess = res.data.success
+        this.resOfSending()
       }).catch(err => {
         console.log('Error' + err)
       })
+    },
+    resOfSending() {
+      if (this.isPostSuccess) {
+        this.showModal()
+      } else {
+        alert('Ошибка отправки заявки')
+      }
     }
 
   }
 }
-
 </script>
 
 <style>
@@ -185,11 +161,11 @@ export default {
   box-shadow: gray 0 0 3px;
   padding: 10px;
 }
-.reqiured {
- color: red;
+.first {
+  margin-left: 10px;
 }
-.select {
-
+.reqiured {
+  color: red;
 }
 .check {
   margin-right: 1px;
@@ -197,28 +173,34 @@ export default {
   padding-right: 5px;
 }
 .label {
+  margin-left: 10px;
   padding-left: 5px;
 }
 .second {
+  margin-left: 10px;
   margin-top: 25px;
 }
 .control {
   margin-top: 5px;
 }
 .third {
+  margin-left: 10px;
   margin-top: 25px;
 }
 .textarea {
-  width: 295px;
+  width: 1250px;
 }
 .fileimport {
+  margin-left: 10px;
   border: gray 1px solid;
   margin-bottom: 15px;
 }
 .textfileimport {
+  margin-left: 10px;
   margin-bottom: 0;
 }
 .btn {
+  margin-left: 10px;
   background-color: #1976d2;
   color: white;
 }
